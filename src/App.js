ebf17470d4dd519a25fa02510c05024b5ff4a0a3
Muhammad Asfar <asfar.abid.chaudhay@gmail.com>
2024-02-22 23:37:29 +0500

    About us page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About.js';
 import NB from './components/NB.js';
 import Textform from './components/Textform.js';
 
@@ -6,10 +7,12 @@ import Textform from './components/Textform.js';
 function App() {
   return (
       <>
-         <NB title="TextUtils" About="About TextUtils"/> 
+          <NB title="TextUtils" About="About TextUtils"/> 
+          {/*
           <div className="container">
             <Textform heading="Enter your text here to analyze"/>
-          </div>
+          </div> */}
+          <About/>
       </>
   );
 }
